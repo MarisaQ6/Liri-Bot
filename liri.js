df29a9c810f5ba02ffef2d12ba72806e0aa4f7e4
Marisa Quijano <miss.quijano@gmail.com>
2019-06-24 15:06:33 -0400

    adding var to liri.js

--- liri.js
+++ liri.js
@@ -1,3 +1,16 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var spotify = new spotify(keys.js)
+var axios = require("axios");
+var moment = require("moment");
+var fs = require ("fs");
+
+var Spotify = require('node-spotify-api');
+var spotify = new spotify(keys.spotify);
+
+var whatToDo = process.argv[2];
+var userInput = process.argv[3];
+
+
+
+
+// continue all packages here
