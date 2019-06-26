9a92e5b3090d30466c9ae15d8753fa23692d2de9
Marisa Quijano <miss.quijano@gmail.com>
2019-06-25 23:58:43 -0400

    working on REadMe

--- README.md
+++ README.md
@@ -4,4 +4,33 @@
 
 How I Started:
 
+* Began researching necessary  API's 
+* Created all files needed for Liri-Bot
+* 
+
+## Technologies Used:
+
+* Javascript
+* Node.js
+
+## NPM Packages:
+
+* Spotify
+* donv
+* request 
+
+### What Liri-Bot is designed to do
+
+* 
+* 
+
+### Author
+
+* Marisa Quijano, Full Stack Web Developer, UCF Coding Boot Camp
+
+#### Special Thanks
+
+* UCF Continuing Education Coding Boot ~~coffee~~ Camp
+
+
 
--- liri.js
+++ liri.js
@@ -10,6 +10,33 @@ var spotify = new spotify(keys.spotify);
 var whatToDo = process.argv[2];
 var userInput = process.argv[3];
 
+function spotifyThis(){
+    // go to npm spotify and copy paste the function and var info add limit if you want 1-10
+    spotify
+        .search({ type: 'track', query: userInput, limit: 5 })
+        .then(function)
+
+}
+
+switch (whatToDo){
+    case "spotify-this-song":
+        spotifyThis()
+        break;
+
+    case "concert-this":
+        concertThis()
+        break;
+
+    case "movie-this":
+        movieThis()
+        break;
+
+    case "do-what-it-says":
+        doWhatItSays()
+        break;
+
+
+}
 
 
 
